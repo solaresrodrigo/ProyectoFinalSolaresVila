a911490f8446c65138a699c2864be00b54e3b08e
Alejandro Vila Torrico <37456>
2018-09-13 15:13:19 -0400

    Añado clases tipo Agencia

---
+++ src/agencia/view/AgenciasIO.java
@@ -0,0 +1,37 @@
+package agencia.view;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import agencia.entity.Agencia;
+import control.Conexion;
+
+public class AgenciasIO {
+
+	private Conexion conexion;
+	private Scanner scanner;
+	public AgenciasIO(Conexion conexion, Scanner scanner) {
+		this.conexion = conexion;
+		this.scanner = scanner;
+	}
+	
+	public void añadir() {
+		Agencia agencia = AgenciaIO.ingresar(scanner);
+		
+		try {
+			conexion.consulta("INSERT INTO AGENCIA(DIRECCION, CIUDAD)"
+					+ "VALUES(?,?)" );
+			
+			conexion.getSentencia().setString(1,agencia.getDireccion());
+			conexion.getSentencia().setString(2,agencia.getCiudad());
+			
+			conexion.modificacion();
+			conexion.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch(Throwable e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
